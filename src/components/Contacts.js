cc1416b66b94ed1119a3a8345c22a501fe121b3f
wrightw <wrightw2055@gmail.com>
2020-06-23 14:25:11 -0400

    building components

---
+++ src/components/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contacts() {
+    return (
+        <div>
+            <ul>
+                <li>Email</li>
+                <li>Phone</li>
+                <li>Social Media</li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Contacts
